63cfe852de236732dfecc38baf4f7b900a80687c
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2025-08-02 17:22:02 +0600

    Update

--- js/custom.js
+++ js/custom.js
@@ -51,36 +51,37 @@
             $(".tp-offcanvas-overlay").removeClass("tp-offcanvas-overlay-open");
         });
 
-        // Who we are
-        function whoWe(){
-            jQuery(document).ready(function ($) {
-                $('.process-step .step-header').on('click', function () {
+        // The Plan 
+        function thePlan() {
+                $('.process-step:not(.active) .step-content').hide();
+                $('.process-step.active .step-content').show();
+                $('.process-step.active .step-header .arrow-icon').removeClass('fa-angle-down').addClass('fa-angle-up');
+                $('.process-step .step-header').on('click', function (e) {
                     const $parentStep = $(this).closest('.process-step');
                     const $content = $parentStep.find('.step-content');
                     const $arrow = $parentStep.find('.arrow-icon');
-
-                    // Close all other open steps and reset their icons
-                    $('.process-step').not($parentStep).removeClass('active').find('.step-content').slideUp();
-                    $('.process-step').not($parentStep).find('.arrow-icon').removeClass('fa-angle-up').addClass('fa-angle-down'); // Reset other icons
-
-                    // Toggle the clicked step
-                    $parentStep.toggleClass('active');
-                    $content.slideToggle(300); // Smooth slide animation
-
-                    // Change Font Awesome icon based on active state
                     if ($parentStep.hasClass('active')) {
-                        $arrow.removeClass('fa-angle-down').addClass('fa-angle-up'); // Change to angle-up
+                        $parentStep.removeClass('active');
+                        $content.slideUp(300); 
+                        $arrow.removeClass('fa-angle-up').addClass('fa-angle-down');
                     } else {
-                        $arrow.removeClass('fa-angle-up').addClass('fa-angle-down'); // Change back to angle-down
+                        $('.process-step.active').not($parentStep).each(function () {
+                            const $otherStep = $(this);
+                            const $otherContent = $otherStep.find('.step-content');
+                            const $otherArrow = $otherStep.find('.arrow-icon');
+                            $otherStep.removeClass('active');
+                            $otherContent.slideUp(300);
+                            $otherArrow.removeClass('fa-angle-up').addClass('fa-angle-down');
+                        });
+                        $parentStep.addClass('active');
+                        $content.slideDown(300); 
+                        $arrow.removeClass('fa-angle-down').addClass('fa-angle-up');
                     }
                 });
-
-                // Automatically set the correct icon for the step with the 'active' class on page load
-                $('.process-step.active .step-content').slideDown();
-                $('.process-step.active .step-header .arrow-icon').removeClass('fa-angle-down').addClass('fa-angle-up');
-            });
+            
         }
-        whoWe()
+        thePlan();
+       
         // Verified Talent
         function verifiedTalent(){
                 $('.accordion-header').on('click', function () {
@@ -118,293 +119,10 @@
             });
         }
         textSlider()
-        // $('.review_slider').slick({
-        //     slidesToShow: 1,
-        //     slidesToScroll: 1,
-        //     // autoplay: true,
-        //     // autoplaySpeed: 4000,
-        //     arrows: true,
-        //     dots: false,
-        //     infinite: true,
-        //     prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
-        //     nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
-        //     // centerMode: true,
-        //     // centerPadding: "20px",
-        //     // initialSlide: 2,
-            
-        // });
-        $('.movies_card_wrapper-2').slick({
-            slidesToShow: 6,
-            slidesToScroll: 1,
-            // autoplay: true,
-            // autoplaySpeed: 4000,
-            arrows: true,
-            dots: false,
-            infinite: true,
-            // centerMode: true,
-            // centerPadding: "20px",
-            // initialSlide: 2,
-            prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
-            nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
-            responsive: [
-                { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-                { breakpoint: 767, settings: { slidesToShow: 1, slidesToScroll: 1 } }
-            ]
-        });
-        $('.trending_movie_card').slick({
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            arrows: true,
-            dots: false,
-            infinite: true,
-            // centerMode: true,
-            // centerPadding: "20px",
-            // initialSlide: 2,
-            prevArrow: `<span class="left-arrow"><i class="fa-solid fa-angle-left"></i></span>`,
-            nextArrow: `<span class="right-arrow"><i class="fa-solid fa-angle-right"></i></span>`,
-            responsive: [
-                { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-                { breakpoint: 767, settings: { slidesToShow: 1, slidesToScroll: 1 } }
-            ]
-        });
-        $(document).ready(function () {
-            $('.testimonial-slider').slick({
-                slidesToShow: 2.3,
-                arrows: false,
-                autoplay: true,
-                dots: true,
-                dotsClass: "testomonial_dot",
-                responsive: [
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 1,
-                        }
-                    }
-                ]
-            });
-        });
-        function animateAboutCardWrapper() {
-            const aboutCardWrappers = document.querySelectorAll(".service_card_animated");
-            if (aboutCardWrappers.length > 0) {
-                gsap.from(aboutCardWrappers, {
-                    scrollTrigger: {
-                        trigger: ".service_card_ani",
-                        start: "top 80%",
-                        toggleActions: "play none none reverse",
-                    },
-                    opacity: 0,
-                    y: 50,
-                    duration: 1,
-                    stagger: 0.3,
-                });
-            }
-        }
-        function animatePackagesTitleWrapper() {
-            const packagesTitleWrapper = document.querySelectorAll(".pricing");  // Select all elements
-            if (packagesTitleWrapper.length > 0) {
-                gsap.from(packagesTitleWrapper, {
-                    scrollTrigger: {
-                        trigger: ".our_pricign_plan",
-                        start: "-500px 80%",
-                        toggleActions: "play none none reverse",
-                    },
-                    opacity: 0,
-                    y: 50,
-                    duration: 1,
-                    stagger: 0.3,
-                });
-            }
-        }
-        animatePackagesTitleWrapper()
-        $('.blog_card_wrapper').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            arrows: true,
-            dots: true,
-            infinite: true,
-            dotsClass: "review_dots",
-            prevArrow: `<span class="left-arrow"><i class="fa fa-undo"></i></span>`,
-            nextArrow: `<span class="right-arrow"><i class="fa-solid fa-rotate-right"></i></span>`,
-            responsive: [
-                { breakpoint: 1300, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-                { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-                { breakpoint: 767, settings: { slidesToShow: 1, slidesToScroll: 1 } },
-            ]
-        });
-        // $('.clients_review_slider').slick({
-        //     slidesToShow: 1,
-        //     slidesToScroll: 1,
-        //     autoplay: true,
-        //     autoplaySpeed: 4000,
-        //     arrows: true,
-        //     dots: true,
-        //     dotsClass: "review_dots",
-        //     infinite: true,
-        //     prevArrow: `<span class="left-arrow"><i class="fa fa-undo"></i></span>`,
-        //     nextArrow: `<span class="right-arrow"><i class="fa-solid fa-rotate-right"></i></span>`,
-        // });
-        $('.client_feedback_wrapper').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            arrows: true,
-            dots: false,
-            // dotsClass: "review_dots",
-            infinite: true,
-            prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
-            nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
-        });
+        
+      
 
-        // var $cleaningSlider = $('.second_service');
-        $('.second_service').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            dots: true,
-            autoplay: true,
-            autoplaySpeed: 1000,
-            speed: 3000,
-            arrows: false,
-            touchThreshold: 10,
-            dotsClass: 'service_dot',
-            responsive: [
-                { breakpoint: 1300, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-                { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-                { breakpoint: 767, settings: { slidesToShow: 1, slidesToScroll: 1 } },
-            ]
-        });
-        // Post Gallery Slider
-        $('.post-gallery').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            fade: true,
-            autoplaySpeed: 4000,
-            prevArrow: `<span class="left-arrow"><i class="fa-solid fa-arrow-left"></i></span>`,
-            nextArrow: `<span class="right-arrow"><i class="fa-solid fa-arrow-right"></i></span>`,
-            speed: 2000,
-            easing: 'ease-in-out',
-        });
-        $('.minecraft_movie').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            fade: true,
-            arrows: false,
-            dots: true,
-            dotsClass:"minecraft",
-            autoplaySpeed: 4000,
-            speed: 2000,
-            easing: 'ease-in-out',
-        });
-        $(document).ready(function () {
-            const $faqs = $(".single_enhanced");
-            // Set second FAQ active initially
-            if ($faqs.length > 0) {
-                $faqs.eq(1).addClass("active");
-                // $faqs.eq(0).addClass("")
-                $faqs.eq(1).find(".answer").css("max-height", $faqs.eq(1).find(".answer")[0].scrollHeight + "px");
-                $faqs.eq(1).find(".icon").text("−");
-            }
-            $faqs.on("click", function () {
-                const $this = $(this);
-                const $answer = $this.find(".answer");
-                const $icon = $this.find(".icon");
-
-                if (!$this.hasClass("active")) {
-                    $faqs.removeClass("active").find(".answer").css("max-height", "0");
-                    $faqs.find(".icon").text("+");
-
-                    $this.addClass("active");
-                    $answer.css("max-height", $answer[0].scrollHeight + "px");
-                    $icon.text("−");
-                } else {
-                    $this.removeClass("active");
-                    $answer.css("max-height", "0");
-                    $icon.text("+");
-                }
-            });
-        });
-        $(document).ready(function () {
-            const $faqs = $(".single_faq");
-            if ($faqs.length > 0) {
-                $faqs.eq(1).addClass("active");
-                $faqs.eq(1).find(".answer").css("max-height", $faqs.eq(1).find(".answer")[0].scrollHeight + "px");
-                $faqs.eq(1).find(".icon").text("−");
-            }
-            $faqs.on("click", function () {
-                const $this = $(this);
-                const $answer = $this.find(".answer");
-                const $icon = $this.find(".icon");
-
-                if (!$this.hasClass("active")) {
-                    $faqs.removeClass("active").find(".answer").css("max-height", "0");
-                    $faqs.find(".icon").text("+");
-
-                    $this.addClass("active");
-                    $answer.css("max-height", $answer[0].scrollHeight + "px");
-                    $icon.text("−");
-                } else {
-                    $this.removeClass("active");
-                    $answer.css("max-height", "0");
-                    $icon.text("+");
-                }
-            });
-        });
        
-        var $cleaningSlider = $('.expert_slider');
-        $cleaningSlider.slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            arrows: false,
-            // autoplay: true,
-            autoplaySpeed: 2000,
-            speed: 2000,
-            arrows: true,
-            prevArrow: `<span class="left-arrow"><i class="fa-solid fa-angle-left"></i></span>`,
-            nextArrow: `<span class="right-arrow"><i class="fa-solid fa-angle-right"></i></span>`,
-            easing: 'ease-in-out',
-            responsive: [
-                { breakpoint: 1190, settings: { slidesToShow: 3 } },
-                { breakpoint: 924, settings: { slidesToShow: 2 } },
-                { breakpoint: 600, settings: { slidesToShow: 1 } }
-            ]
-        });
-        var $cleaningSlider = $('.single_iteam ul');
-        $cleaningSlider.slick({
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            arrows: false,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            speed: 2000,
-            dots: false,
-            easing: 'ease-in-out',
-            // responsive: [
-            //     { breakpoint: 1190, settings: { slidesToShow: 3 } },
-            //     { breakpoint: 924, settings: { slidesToShow: 2 } },
-            //     { breakpoint: 600, settings: { slidesToShow: 1 } }
-            // ]
-        });
-        $('.blog_post_slider').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            arrows: false,
-            autoplay: true,
-            dots: true,
-            dotsClass: "testomonial_dot",
-            responsive: [
-                { breakpoint: 1190, settings: { slidesToShow: 3 } },
-                { breakpoint: 924, settings: { slidesToShow: 1 } },
-                { breakpoint: 600, settings: { slidesToShow: 1 } }
-            ]
-        });
         // Scroll-triggered Counter Animation
         $(".about_count").each(function () {
             let counter = $(this);
@@ -438,38 +156,6 @@
         });
         $(".back_top").on("click", function () {
             $("html, body").animate({ scrollTop: 0 }, "slow");
-        });
-        // Categories Hover Effects
-        $('.categories_area.style_1 ul li a').each(function () {
-            let link = $(this);
-            link.on("mouseenter", function () {
-                link.find('.download_btn img').attr('src', './image/service_details/icon5.png');
-                link.find('.pdf_btn img').attr('src', './image/service_details/icon2.png');
-            });
-            link.on("mouseleave", function () {
-                link.find('.download_btn img').attr('src', './image/service_details/icon4.png');
-                link.find('.pdf_btn img').attr('src', './image/service_details/icon1.png');
-            });
-        });
-        $('.search_card_btn .buy_card .buy_card_icon').each(function () {
-            let link = $(this);
-
-            link.on("mouseenter", function () {
-                link.find('img').attr('src', './image/header/icon3.png');
-            });
-            link.on("mouseleave", function () {
-                link.find('img').attr('src', './image/header/icon1.png');
-                
-            });
-        });
-        $('.search_card_btn .buy_card .search_icon').each(function () {
-            let link = $(this);
-            link.on("mouseenter", function () {
-                link.find('img').attr('src', './image/header/icon4.png');
-            });
-            link.on("mouseleave", function () {
-                link.find('img').attr('src', './image/header/icon2.png');
-            });
         });
         //  Magnific Popup Configuration
         $('.playBtn').magnificPopup({
@@ -507,23 +193,6 @@
             }
         });
     
-        // Progress Bar Animation
-        $(".progress-fill").each(function (index) {
-            let bar = $(this);
-            let value = bar.data("value");
-            let valueText = $(".progress-value").eq(index);
-            gsap.to(bar, {
-                width: value + "%",
-                duration: 2,
-                ease: "power2.out"
-            });
-            gsap.to(valueText[0], {
-                innerHTML: value + "%",
-                duration: 2,
-                snap: { innerHTML: 1 },
-                ease: "power2.out"
-            });
-        });
         $(document).ready(function () {
             $('.search_icon').on('click', function () {
                 $('#search').addClass('search-active');
